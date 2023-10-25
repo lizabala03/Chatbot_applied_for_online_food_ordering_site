8b6a1ba9517d4071f347c03b94ef6b7253aa7b7a
Liza Bala <lizabala03@gmail.com>
2023-10-25 12:45:31 +0600

    setting.py

---
+++ foodie_master/foodie/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+      username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Username"}))
+      email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"Email"}))
+      password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
+      password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Confirm Password"}))
+
---
+++ foodie_master/foodie/views.py
@@ -0,0 +1,42 @@
+from urllib import request
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate,logout
+from django.contrib import messages
+from django.conf import settings
+from django.contrib.auth.models import User
+
+def home(request):
+    return render(request, 'index.html' )
+
+def register_view(request):
+    
+        if request.method == "POST":
+            username = request.POST['uname']
+            pass1 = request.POST['psw']
+            pass2 = request.POST['psw-repeat']
+            print(username)
+            if pass1 != pass2:
+                messages.error(request,"Password didn't match!")
+            else:
+                myuser = User.objects.create_user(username=username, password=pass1)
+                myuser.is_active=True
+                myuser.save()
+                return redirect('/login')
+        return render(request, "sign-up.html")
+
+
+    
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            return redirect('http://localhost:63342/chatbotfood/foodie-master/index.html?_ijt=3mvcr5777d2hdfp9hogm52nj56')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+    context = {}
+    return render(request, 'login.html')
